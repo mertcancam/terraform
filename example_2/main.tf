bac78f6a0a530f7561f3e412faa9662f61dd46b0
Mustafa Mertcan CAM <mustafam@wolfram.com>
2021-09-26 08:37:16 -0500

    use module for ec2 && switch to us west region

--- example_2/main.tf
+++ example_2/main.tf
@@ -20,13 +20,15 @@ resource "aws_efs_mount_target" "mert_target_group" {
   security_groups = ["${aws_security_group.efs.id}"]
 }
 
-resource "aws_instance" "mert_app" {
-  count           = 2
-  ami             = data.aws_ami.ubuntu.id
-  instance_type   = "t3.micro"
-  key_name        = "mert"
-  security_groups = ["${aws_security_group.efs.id}"]
+module "ec2_instance" {
+  count                  = 2
+  source                 = "terraform-aws-modules/ec2-instance/aws"
+  version                = "~> 3.0"
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t3.micro"
+  key_name               = "mert"
   subnet_id       = local.subnet_id
+  vpc_security_group_ids = ["${aws_security_group.efs.id}"]
 
   tags = {
     Name = "Mert EFS server ${count.index}"
